36714e762240b6477290563702abd5066e03b14e
tajaywhittaker06-dotcom <tajaywhittaker06@gmail.com>
2026-08-26 21:16:45 -0500

    Fix syntax error in server.py

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def account():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "10000"))
-    app.run(host="0.0.0.0", port=port)    })
+    app.run(host="0.0.0.0", port=port)    
 
 
 @app.route("/account")
